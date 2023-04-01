9cfb8f84b37807eca839959fcf2c0e15d0482187
Assem Mohamed <129564950+Assem29@users.noreply.github.com>
2023-04-02 00:03:23 +0200

    first commit

---
+++ src/components/common/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../../styles/Title.scss";
+
+const Title = ({title}) => {
+  return (
+    <div className='sc-title text-center'>
+        <h3 className='text-capitalize'>{title}</h3>
+        
+    </div>
+  )
+}
+
+export default Title;
